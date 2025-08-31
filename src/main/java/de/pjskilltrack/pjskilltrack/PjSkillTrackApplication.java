1e00c29030af3d80862044c69a8d7c04e2e69bdd
Luca Witt <lucawitt02@gmail.com>
2025-08-31 14:59:37 +0200

    Initial commit

---
+++ src/main/java/de/pjskilltrack/pjskilltrack/PjSkillTrackApplication.java
@@ -0,0 +1,15 @@
+package de.pjskilltrack.pjskilltrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjSkillTrackApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(PjSkillTrackApplication.class, args);
+    }
+
+}
